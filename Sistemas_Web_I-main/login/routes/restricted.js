cc0e8ed27b933616505a6c031603cc60401f205c
Javier Martinez <javimartinezcristobal@gmail.com>
2024-12-18 11:57:51 +0100

    Ejemplos de todo tipo

--- Sistemas_Web_I-main/login/routes/restricted.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-  res.render('restricted', {user: req.session.user});
-});
-
-module.exports = router;
